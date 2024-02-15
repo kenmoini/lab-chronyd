e208af2d6753781b75c4157f66c8f81511ce0c61
Ken Moini <ken@kenmoini.com>
2024-02-14 23:34:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Container as a Service - NTP (Chronyd)
+
+## Installation
+
+```bash
+git clone https://github.com/kenmoini/lab-chronyd
+
+cd lab-chronyd
+
+# Modify config/chrony.conf as needed
+
+cp lab-chronyd.service /etc/systemd/system/
+
+systemctl daemon-reload
+
+systemctl enable --now lab-chronyd.service
+```
---
+++ scripts/service_init.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -x
+
+CONTAINER_NAME="lab-chronyd"
+CONTAINER_VOLUME_ROOT="/opt/${CONTAINER_NAME}"
+CONTAINER_IMAGE="docker.io/publicarray/chrony:latest"
+CONTAINER_NETWORK_NAME="host"
+CONTAINER_RESOURCE_LIMITS="-m 64m"
+CONTAINER_PORTS="-p 123/udp -p 323/udp"
+CONTAINER_VOLUMES="-v ${CONTAINER_VOLUME_ROOT}/config/chrony.conf:/etc/chrony.conf:Z"
+
+################################################################################### EXECUTION PREFLIGHT
+## Ensure there is an action arguement
+if [ -z "$1" ]; then
+  echo "Need action arguement of 'start', 'restart', or 'stop'!"
+  echo "${0} start|stop|restart"
+  exit 1
+fi
+
+################################################################################### SERVICE ACTION SWITCH
+case $1 in
+
+  ################################################################################# RESTART/STOP SERVICE
+  "restart" | "stop" | "start")
+    echo "Stopping container services if running..."
+
+    echo "Killing ${CONTAINER_NAME} container..."
+    /usr/bin/podman kill ${CONTAINER_NAME}
+
+    echo "Removing ${CONTAINER_NAME} container..."
+    /usr/bin/podman rm -f -i ${CONTAINER_NAME}
+    ;;
+
+esac
+
+case $1 in
+
+  ################################################################################# RESTART/START SERVICE
+  "restart" | "start")
+
+    echo "Pulling container image..."
+    podman pull ${CONTAINER_IMAGE}
+
+    echo "Starting container services..."
+
+    # Deploy container
+    echo -e "Deploying ${CONTAINER_NAME}...\n"
+    podman run -dt \
+    --network "${CONTAINER_NETWORK_NAME}" ${CONTAINER_PORTS} \
+    --name ${CONTAINER_NAME} \
+    ${CONTAINER_RESOURCE_LIMITS} \
+    ${CONTAINER_IMAGE}
+
+    ;;
+
+esac
